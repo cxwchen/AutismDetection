26328f2d532e4490ad2e4430015411c961539b06
cxwchen <100145127+cxwchen@users.noreply.github.com>
2025-05-07 12:32:11 +0200

    Update classifiers.py

--- classification/src/classifiers.py
+++ classification/src/classifiers.py
@@ -1,8 +1,6 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.metrics import make_scorer, precision_score, recall_score, f1_score, accuracy_score, classification_report, confusion_matrix, balanced_accuracy_score
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Flatten
@@ -11,7 +9,7 @@ from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
 
 def applySVM(feat_train, feat_test, y):
-    model = SVC()
+    model = SVC() #here we should use the parameters we found in hyperparametertuning.py
     model.fit(feat_train, y)
     ypred = model.predict(feat_test)
     return ypred
