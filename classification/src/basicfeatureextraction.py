0a828c64e3dfcd63cb7815507b32043065688090
cxwchen <100145127+cxwchen@users.noreply.github.com>
2025-05-08 18:39:18 +0200

    changes to classification files

--- classification/src/basicfeatureextraction.py
+++ classification/src/basicfeatureextraction.py
@@ -1,10 +1,16 @@
 import numpy as np
 import pandas as pd
 import seaborn as sns
+from loaddata import load_features
+from sklearn.model_selection import train_test_split
 
-data = pd.read_csv("jfdkjl")
+ffilepath = input("Please enter the filepath to the female data: ")
+mfilepath = input("Please enter the filepath to the male data: ")
+fdata = load_features(ffilepath)
+mdata = load_features(mfilepath)
 
 #train-test split blabla
+
 
 
 #Standard normalization blabla
--- classification/src/loaddata.py
+++ classification/src/loaddata.py
@@ -3,3 +3,4 @@ import pandas as pd
 def load_features(file_path):
     features = pd.read_csv(file_path)
     return features
+
