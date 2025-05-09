531122e01bf8567c0006c3fe7a60abb478fcd1c9
H4naka <102389323+H4naka@users.noreply.github.com>
2025-05-09 12:16:22 +0200

    RandForrest

--- classification/src/classifiers.py
+++ classification/src/classifiers.py
@@ -7,6 +7,8 @@ from keras.layers import Flatten
 from sklearn.neural_network import MLPClassifier
 from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 def applySVM(feat_train, feat_test, y):
     model = SVC() #here we should use the parameters we found in hyperparametertuning.py
@@ -16,6 +18,12 @@ def applySVM(feat_train, feat_test, y):
 
 def applyLogR(feat_train, feat_test, y):
     model = LogisticRegression()
+    model.fit(feat_train, y)
+    ypred = model.predict(feat_test)
+    return ypred
+
+def applyRandForrest(feat_train, feat_test,y):
+    model = RandomForestClassifier()
     model.fit(feat_train, y)
     ypred = model.predict(feat_test)
     return ypred
