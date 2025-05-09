8cf3675ba2b958ae9e21856f8cfb6c593a971937
cxwchen <100145127+cxwchen@users.noreply.github.com>
2025-05-09 13:06:36 +0200

    add proper documentation

--- classification/src/classifiers.py
+++ classification/src/classifiers.py
@@ -1,3 +1,11 @@
+"""Classifier methods
+
+This script blabla
+
+
+
+"""
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -9,27 +17,129 @@ from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.neural_network import MLPClassifier
 
-def applySVM(feat_train, feat_test, y):
+
+def applySVM(feat_train, y):
+    """
+    -----------------------------------------------------------------------------------------
+    This function applies Support Vector Machine (SVM) from sklearn on the training features
+    -----------------------------------------------------------------------------------------
+
+    Parameters
+    ----------
+    feat_train : 
+        The training features
+    y : 
+        The true labels
+    
+    Returns
+    -------
+    model : object
+        The trained SVM model
+    
+    """
+
     model = SVC() #here we should use the parameters we found in hyperparametertuning.py
     model.fit(feat_train, y)
     # ypred = model.predict(feat_test)
     return model
 
-def applyLogR(feat_train, feat_test, y):
+def applyLogR(feat_train, y):
+    """
+    -----------------------------------------------------------------------------------------
+    This function applies Logistic Regression (LR) from sklearn on the training features
+    -----------------------------------------------------------------------------------------
+
+    Parameters
+    ----------
+    feat_train : 
+        The training features
+    y : 
+        The true labels
+    
+    Returns
+    -------
+    model : object
+        The trained LR model
+    
+    """
+
     model = LogisticRegression()
     model.fit(feat_train, y)
     # ypred = model.predict(feat_test)
     return model
 
-def applyRandForest(feat_train, feat_test,y):
+def applyRandForest(feat_train, y):
+    """
+    -----------------------------------------------------------------------------------------
+    This function applies Random Forest (RF) from sklearn on the training features
+    -----------------------------------------------------------------------------------------
+
+    Parameters
+    ----------
+    feat_train : 
+        The training features
+    y : 
+        The true labels
+    
+    Returns
+    -------
+    model : object
+        The trained RF model
+    
+    """
     model = RandomForestClassifier()
     model.fit(feat_train, y)
     # ypred = model.predict(feat_test)
     return model
 
-def applyDT(feat_train, feat_test, y):
+def applyDT(feat_train, y):
+    """
+    -----------------------------------------------------------------------------------------
+    This function applies Decision Tree (DT) from sklearn on the training features
+    -----------------------------------------------------------------------------------------
+
+    Parameters
+    ----------
+    feat_train : 
+        The training features
+    y : 
+        The true labels
+    
+    Returns
+    -------
+    model : object
+        The trained DT model
+    
+    """
+        
     model = DecisionTreeClassifier()
     model.fit(feat_train, y)
     # ypred = model.predict(feat_test)
     return model
+
+def applyMLP(feat_train, y):
+    """
+    -----------------------------------------------------------------------------------------
+    This function applies Multi-Layer Perceptron (MLP) from sklearn on the training features
+    -----------------------------------------------------------------------------------------
+
+    Parameters
+    ----------
+    feat_train : 
+        The training features
+    y : 
+        The true labels
+    
+    Returns
+    -------
+    model : object
+        The trained MLP model
+    
+    """
+
+    model = MLPClassifier() #default: one layer with 100 units
+    model.fit(feat_train, y)
+    return model
+
