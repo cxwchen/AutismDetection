fbf043906900e1abb40f15a5cf7c8dd23aba136a
Fkrete <131753232+fkrete@users.noreply.github.com>
2025-05-27 10:25:33 +0200

    Update cluster.py
    removed unused random forest classifier

--- featureselection/src/cluster.py
+++ featureselection/src/cluster.py
@@ -43,9 +43,6 @@ def load_file():
 def cluster(X, y, t = 1):
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 
-    clf = RandomForestClassifier(n_estimators=100, random_state=42)
-    clf.fit(X_train, y_train)
-
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 8))
     corr = spearmanr(X).correlation
 
